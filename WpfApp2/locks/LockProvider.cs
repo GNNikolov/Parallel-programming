b1cba962463fd3b5851e3aff0a2c918eebf6bbe8
Joro <nikolov@alfastack.bg>
2020-01-05 19:02:21 +0100

    Packages and other files

--- WpfApp2/locks/LockProvider.cs
+++ WpfApp2/locks/LockProvider.cs
@@ -1,8 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace WpfApp2.locks
 {
