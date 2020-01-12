6b18281f1fcc81d84ef87a52fb458bb8e43e6696
Joro <nikolov@alfastack.bg>
2020-01-12 17:20:40 +0100

    Added Cached file with data

--- WpfApp2/locks/deadlock/DeadLock.cs
+++ WpfApp2/locks/deadlock/DeadLock.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -16,7 +19,14 @@ namespace WpfApp2.deadlock
         {
             await base.showLock(enableLock);
             mWindow.items.Items.Add("Started...");
-            Task<string> mTask = getJsonAsync();
+            Task<string> mTask = null;
+            try
+            {
+                mTask = getJsonAsync();
+            }
+            catch (Exception e) {
+                mTask = loadCacheAsync();
+            }
             if (!enableLock)
             {
                 await mTask;
@@ -27,17 +37,35 @@ namespace WpfApp2.deadlock
                 mWindow.items.Items.Add(data.employee_name);
             }
         }
+
         private async Task<string> getJsonAsync()
         {
             var httpClient = new HttpClient();
             var mResult = await httpClient.GetStringAsync(URL);
-          
+            if (mResult == null) {
+                throw new Exception("No data fetched!");
+            }
             return mResult;
+        }
+
+        private async Task<string> loadCacheAsync() {
+            var path = @"D:\\Workspace\\МЕИ\\Паралелно програмиране\\kursov_proekt\\WpfApp2\\cache.txt";
+            var reader = File.OpenText(path);
+            await Task.Delay(300);
+            var result = await reader.ReadToEndAsync();
+            return result;
         }
 
         private List<models.Employee> decodeData(string json)
         {
+            var jobject = JsonConvert.DeserializeObject<JObject>(json);
+            if (jobject.ContainsKey("data")) {
+                var data = jobject.GetValue("data").ToString();
+                return JsonConvert.DeserializeObject<List<models.Employee>>(data);
+            }
             return JsonConvert.DeserializeObject<List<models.Employee>>(json);
         }
+
+      
     }
 }
